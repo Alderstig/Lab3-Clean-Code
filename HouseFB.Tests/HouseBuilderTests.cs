e7b240bca34f28d1b2947e60032e098fb0436fd8
Philip Alderstig <alderstig@hotmail.com>
2021-12-19 15:56:18 +0100

    added tests in HouseFB.Tests

--- HouseFB.Tests/HouseBuilderTests.cs
+++ HouseFB.Tests/HouseBuilderTests.cs
@@ -11,10 +11,20 @@ namespace HouseFB.Tests
     public class HouseBuilderTests
     {
         private HouseBuilder _builder;
+        private House _house;
+
         [SetUp]
         public void Setup()
         {
-            _builder = new HouseBuilder();
+             _builder = new HouseBuilder();
+
+            _house = _builder
+                .SetNoOfRooms(1)
+                .SetNoOfWindows(1)
+                .SetStreetAdress("MyAdress 1")
+                .HasSwimmingPool(false)
+                .SetNoOfParkingSpots(1)
+                .Build();
         }
 
         [Test]
@@ -28,6 +38,46 @@ namespace HouseFB.Tests
                     .SetNoOfParkingSpots(1);
 
             Assert.DoesNotThrow(() => builder.Build());
+        }
+
+        [Test]
+        public void SetNoOfRooms_GivenHouseBuilder_ReturnsTrueIfMatchingExpectedNoOfRooms()
+        {
+            int expectedNoOfRooms = 1;
+
+            Assert.AreEqual(expectedNoOfRooms, _house.NoOfRooms);
+        }
+
+        [Test]
+        public void SetNoOfWindows_GivenHouseBuilder_ReturnsTrueIfMatchingExpectedNoOfWindows()
+        {
+            int expectedNoOfWindows = 1;
+
+            Assert.AreEqual(expectedNoOfWindows, _house.NoOfWindows);
+        }
+
+        [Test]
+        public void SetStreetAdress_GivenHouseBuilder_ReturnsTrueIfMatchingExpectedStreetAdress()
+        {
+            string expectedStreetAdress = "MyAdress 1";
+
+            Assert.AreEqual(expectedStreetAdress, _house.StreetAdress);
+        }
+
+        [Test]
+        public void HasSwimmingPool_GivenHouseBuilder_ReturnsTrueIfMatchingExpectedBool()
+        {
+            bool expectedHasSwimmingPool = false;
+
+            Assert.AreEqual(expectedHasSwimmingPool, _house.HasSwimmingPool);
+        }
+
+        [Test]
+        public void SetNoOfParkingSpots_GivenHouseBuilder_ReturnsTrueIfMatchingExpectedNoOfParkingSpots()
+        {
+            int expectedNoOfParkingSpots = 1;
+
+            Assert.AreEqual(expectedNoOfParkingSpots, _house.ParkingSpotsInGarage);
         }
     }
 }
